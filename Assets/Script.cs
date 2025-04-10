b79ee41dcea1a4838991d69aa3b06495dc24a3f0
MeliKeni <melinakenigsberg@gmail.com>
2025-04-10 11:29:24 -0300

    TERMINADO

---
+++ Assets/Script.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string codigoSabor;
+    public float cantidadHelado;
+    float costo;
+
+
+    void Start()
+    {
+        if (codigoSabor != "DDL" && codigoSabor != "CHO" && codigoSabor != "FRU" || cantidadHelado < 250 || cantidadHelado > 3000)
+        {
+            
+                Debug.Log("El gusto ingresado o la cantidad de helado pedida son incorrectos");
+                return;
+            
+        }    else
+        {
+
+            costo = cantidadHelado * 500;
+
+            if (codigoSabor == "FRU")
+            {
+                costo = costo * 0.9f;
+            }
+
+            Debug.Log("El costo es de " + costo + "$");
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
